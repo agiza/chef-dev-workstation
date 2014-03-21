8b7f1d583dbb2617b043bf4eddaa3970442d9c1a
Sean Carolan <scarolan@gmail.com>
2014-03-21 12:05:27 -0500

    Added fully-featured vim to packages list.

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,7 +14,7 @@ case node['platform']
 when 'centos', 'redhat', 'scientific', 'oracle', 'amazon'
   default['epel_yum_mirror_url'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
   default['epel_gpg_key_url'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-  default['package_list'] = %w{git libxml2-devel libxslt-devel nano emacs}
+  default['package_list'] = %w{git libxml2-devel libxslt-devel nano emacs vim-enhanced}
 when 'debian', 'ubuntu'
-  default['package_list'] = %w{git libxml2-dev libxslt-dev nano emacs}
+  default['package_list'] = %w{git libxml2-dev libxslt-dev nano emacs vim}
 end
