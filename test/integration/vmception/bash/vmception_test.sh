9589f56a6359da2f9f80ace11bfc9a79113f9621
Sean Carolan <scarolan@gmail.com>
2014-04-03 10:56:55 -0500

    fixing script so it has the correct suite name.

--- test/integration/vmception/bash/vmception_test.sh
+++ test/integration/vmception/bash/vmception_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script should be run via the busser-bash plugin for test kitchen.
+# It recursively builds self-testing workstations inside nested docker containers.
 
 # First clean up from any previous test runs
 rm -rf chef-dev-workstation
@@ -7,4 +9,4 @@ rm -rf chef-dev-workstation
 su - kitchen -c "git clone https://github.com/scarolan/chef-dev-workstation; cd ~/chef-dev-workstation; berks install; strainer test"
 
 # Finally, spin up another instance inside here and test it too!
-su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.docker.yml .kitchen.yml; kitchen test docker-dind"
+su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.dind.yml .kitchen.yml; kitchen test dind-vmception"
