a0cdc3638251ea86c0f9af8ac86b69c8b6c76bb4
Sean Carolan <scarolan@gmail.com>
2014-04-03 14:28:50 -0500

    Updating test script

--- test/integration/vmception/bash/vmception_test.sh
+++ test/integration/vmception/bash/vmception_test.sh
@@ -8,5 +8,8 @@ rm -rf chef-dev-workstation
 # Next we want to clone the chef-dev-workstation repo, install cookbook dependencies, then run our test suite."
 su - kitchen -c "git clone https://github.com/scarolan/chef-dev-workstation; cd ~/chef-dev-workstation; berks install; strainer test"
 
+# Start up another docker instance inside this one:
+/usr/local/bin/wrapdocker
+
 # Finally, spin up another instance inside here and test it too!
 su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.dind.yml .kitchen.yml; kitchen test vmception-dind"
