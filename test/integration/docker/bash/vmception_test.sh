fefe305548fd9765883eaf578e242a97082792c4
Sean Carolan <scarolan@gmail.com>
2014-04-03 10:21:38 -0500

    fixing suite name.

--- test/integration/docker/bash/vmception_test.sh
+++ test/integration/docker/bash/vmception_test.sh
@@ -7,4 +7,4 @@ rm -rf chef-dev-workstation
 su - kitchen -c "git clone https://github.com/scarolan/chef-dev-workstation; cd ~/chef-dev-workstation; berks install; strainer test"
 
 # Finally, spin up another instance inside here and test it too!
-su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.docker.yml .kitchen.yml; kitchen test default-ubuntu-1204"
+su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.docker.yml .kitchen.yml; kitchen test docker-dind"
