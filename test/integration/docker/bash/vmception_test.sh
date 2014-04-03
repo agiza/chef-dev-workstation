c51fbe4845aeb3140e4e71746ea41a0a15d9db1f
Sean Carolan <scarolan@gmail.com>
2014-04-03 10:19:47 -0500

    updating this to run as the 'kitchen' user

--- test/integration/docker/bash/vmception_test.sh
+++ test/integration/docker/bash/vmception_test.sh
@@ -4,7 +4,7 @@
 rm -rf chef-dev-workstation
 
 # Next we want to clone the chef-dev-workstation repo, install cookbook dependencies, then run our test suite."
-su - vagrant -c "git clone https://github.com/scarolan/chef-dev-workstation; cd ~/chef-dev-workstation; berks install; strainer test"
+su - kitchen -c "git clone https://github.com/scarolan/chef-dev-workstation; cd ~/chef-dev-workstation; berks install; strainer test"
 
 # Finally, spin up another instance inside here and test it too!
-su - vagrant -c "cd ~/chef-dev-workstation; cp .kitchen.docker.yml .kitchen.yml; kitchen test default-ubuntu-1204"
+su - kitchen -c "cd ~/chef-dev-workstation; cp .kitchen.docker.yml .kitchen.yml; kitchen test default-ubuntu-1204"
